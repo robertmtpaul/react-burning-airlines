85e243063080f800dd055214adaaa0278fd97b36
robertmtpaul <rob@glo.id.au>
2020-08-25 16:53:42 +1000

    Imported routes to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Route, Link, Router } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
