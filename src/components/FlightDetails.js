cac87334e6d4dd2417b7d257d48b1214182ef039
robertmtpaul <rob@glo.id.au>
2020-08-26 17:07:51 +1000

    Working reservation selector

--- src/components/FlightDetails.js
+++ src/components/FlightDetails.js
@@ -1,60 +1,60 @@
-import React from 'react';
-import axios from 'axios';
-import { Route, Link, HashRouter as Router } from 'react-router-dom';
+import React from "react";
+import axios from "axios";
+import { Route, Link, HashRouter as Router } from "react-router-dom";
 
 class FlightDetails extends React.Component {
+  state = {
+    data: {
+      reservations: []
+    },
+    user_id: 1,
+    my_reservations: {},
+    bgColour: ""
+  };
 
-    state = {
-        data: {
-            reservations: []
-        },
-        user_id: 1,
-        my_reservations: {
-            seat_id: [],
-            user_id: []
-        },
-        bgColour: ""
-    }
+  componentDidMount() {
+    console.log(this.props);
+    axios
+      .get(
+        `https://rails-burning-airlines.herokuapp.com/flights/${
+          this.props.match.params.id
+        }.json`
+      )
+      .then(data => {
+        console.log(data);
+        this.setState({ data: data.data });
+      })
+      .catch(err => console.log(err));
+  }
 
-    componentDidMount() {
-        console.log(this.props);
-        axios.get(`https://rails-burning-airlines.herokuapp.com/flights/${this.props.match.params.id}.json`)
-            .then(data => {
-                console.log(data);
-                this.setState({ data: data.data })
-            })
-            .catch(err => console.log(err));
+  seatClick = (reservationIndex) => {
+    // this.setState({
+    //   bgColour: "red"
+    // });
+    const newState = this.state;
+    newState.data.reservations[reservationIndex].bgColor = 'red';
+    this.setState(newState);
+  };
 
-    }
-
-    seatClick = (event) => {
-        this.setState({
-            seat_id: this.reservation.id,
-            bgColour: "red"
-        })
-    }
-
-    render() {
-        return (
-
-            <div class="container">
-                {
-                    this.state.data.reservations.map(reservation => (
-                        
-                        <div class="seat" style={{backgroundColor: this.state.bgColour}} onClick={ this.seatClick }>
-                            {reservation.seat_number}
-                        </div>
-
-                    ))
-                }
-            </div>
-
-        )
-    }
-    // array of objects
-    // map through grid of each seat
-    // make grid of divs
-
+  render() {
+    return (
+      <div class="container">
+        {this.state.data.reservations.map((reservation, index) => (
+          <div
+            className="seat"
+            key={index}
+            style={{ backgroundColor: reservation.bgColor }}
+            onClick={() => this.seatClick(index)}
+          >
+            {reservation.seat_number}
+          </div>
+        ))}
+      </div>
+    );
+  }
+  // array of objects
+  // map through grid of each seat
+  // make grid of divs
 }
 
-export default FlightDetails
+export default FlightDetails;
